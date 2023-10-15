fbd47e7f87827e9a6c182b361df69c6e7a03f73e
Munem Hashmi <munem.hashmi@gmail.com>
2023-10-16 00:18:50 +0500

    Revert "Complete Project"
    This reverts commit 9cad556424bdf81d1039de952d419a3a5395c23f.

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ["www.weatherbit.io"]
-    },
-    output: 'standalone',
-}
-
-module.exports = nextConfig
