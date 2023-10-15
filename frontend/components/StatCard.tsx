cff9d2bf6caef60717ef61c97d322dbae6b39203
Munem Hashmi <munem.hashmi@gmail.com>
2023-10-16 00:30:39 +0500

    Complete Project

---
+++ frontend/components/StatCard.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Card, Metric, Text } from "@tremor/react";
+
+type Props = {
+    title: string;
+    metric: string;
+    color?: | "indigo"
+    | "slate"
+    | "gray"
+    | "zic"
+    | "neutral"
+    | "stone"
+    | "read"
+    | "orange"
+    | "amber"
+    | "yellow"
+    | "lime"
+    | "green"
+    | "emerald"
+    | "teal"
+    | "cyan"
+    | "sky"
+    | "blue"
+    | "violet"
+    | "purple"
+    | "fuchsia"
+    | "pink"
+    | "rose"
+    | undefined;
+    
+};
+
+function StatCard({title, metric, color}: Props) {
+  return (
+    <Card decoration="top" decorationColor={color} >
+        <Text>{title}</Text>
+        <Metric>{metric}</Metric>
+    </Card>
+  )
+}
+
+export default StatCard;
